d140657e2cd4c20babab79109fe1e0881b231fe1
SteveWonderbelt <schilds@email.sc.edu>
2021-03-10 12:28:49 -0500

    add vuetify and make landingpage

---
+++ src/components/NavButton.vue
@@ -0,0 +1,17 @@
+<template>
+ <v-btn block elavation="2" class="ma-10" large x-large>
+      {{title}}
+  </v-btn>   
+</template>
+
+<script>
+export default{
+    name: 'NavButton',
+    props:{
+        title:{
+            type: String,
+        },
+    }
+};
+</script>
+
