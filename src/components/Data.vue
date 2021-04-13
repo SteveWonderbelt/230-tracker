d89f5e30964daa179d4dccd4500226deeae54455
schilds427 <schilds427@gmail.com>
2021-04-13 12:06:16 -0400

    4-13-2021

--- src/components/Data.vue
+++ src/components/Data.vue
@@ -10,13 +10,13 @@
     <section v-else>
       <div v-if="loading">Loading...</div>
 
-      <div v-else v-for="currency in info" :key="currency" class="currency">
+      <!-- <div v-else v-for="currency in info" :key="currency" class="currency">
         {{ currency.description }}:
         <span class="lighten">
           <span v-html="currency.symbol"></span
           >{{ currency.rate_float | currencydecimal }}
         </span>
-      </div>
+      </div> -->
     </section>
   </div>
 </template>
@@ -34,6 +34,17 @@ export default {
 
   mounted() {
     axios
+      .get(
+        "https://api.govinfo.gov/collections/BILLS/2021-03-25T20%3A18%3A10Z-04%3A00%3A00?offset=0&pageSize=2&api_key=M0dOXyBYv4LkhdqORrP79V4RgWClPV9PzV4H4wvG"
+      )
+      .then((response) => {
+        this.info = response.data.bpi;
+      })
+      .catch((error) => console.log(error))
+      .finally(() => (this.loading = false));
+  },
+  /* mounted() {
+    axios
       .get("https://api.coindesk.com/v1/bpi/currentprice.json")
       .then((response) => {
         this.info = response.data.bpi;
@@ -45,6 +56,6 @@ export default {
     currencydecimal(value) {
       return value.toFixed(2);
     },
-  },
+  }, */
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -3,12 +3,15 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+import AxiosPlugin from 'vue-axios-cors';
 
 Vue.config.productionTip = false;
+
 
 new Vue({
   router,
   store,
   vuetify,
+  AxiosPlugin,
   render: h => h(App)
 }).$mount("#app");
